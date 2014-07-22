149aa740b86865612b1585cce35e86a836390b15
IvanZorov <i_zoro007@abv.bg>
2014-07-22 22:37:59 +0300

    Update HTML_PDFGen.rb

--- HTML_PDFGen.rb
+++ HTML_PDFGen.rb
@@ -1,23 +1,25 @@
 require "prawn"
+require "cgi"
 require_relative "EquationGenerator"
 
 class HTMLPDFGen
 	@@eqGen = EquationGenerator.new
 	
-	@@allTests = []
+	@@allTests
 
 
 	def GenTests
+		@@allTests = []
 		10.times do |i|
 			@@allTests << @@eqGen.Kvadratno
 		end																
 	end
-	
+
 	def PDFGen name
 		pdf = Prawn::Document.new
 
 		10.times do |i|
-			pdf.text ((i+1).to_s + GetQuest( i-1, 2))
+			pdf.text ((i+1).to_s + GetQuest( i, 2))
 		end
 
 		pdf.render_file ("Test_" + name)
@@ -27,17 +29,68 @@ class HTMLPDFGen
 		pdf = Prawn::Document.new
 
 		10.times do |i|
-			pdf.text ((i+1).to_s + GetQuest( i-1, 2))
-			pdf.text (GetQuest i-1, 0)
-			pdf.text (GetQuest i-1, 1)
+			pdf.text ((i+1).to_s + GetQuest( i, 2))
+			pdf.text (GetQuest i, 0)
+			pdf.text (GetQuest i, 1)
 			pdf.text "\n"
 		end
 
 		pdf.render_file ("Test_" + name.gsub(/.pdf/, "") + "Answers.pdf")
 	end
-	
+
+	def HTMLGen name
+		name+=".html"
+		cgi = CGI.new("html4")
+
+		file =  File.open("Test_" + name.gsub(/.html/, "") + ".html", "w")
+		file.puts cgi.html{ cgi.body{ cgi.h2{"Test " + name.gsub(/.html/, "")} + "\n" + "\n" }  }
+		10.times do |i|
+			file.puts cgi.html {
+				cgi.head{
+					"\n" + cgi.title{"Test " + name.gsub(/.html/, "")}
+				} +
+				cgi.body{
+					"\n" + 
+					cgi.form{ 
+						cgi.p{(i+1).to_s + GetQuest(i, 2)} + "\n"
+					}
+				}
+			}
+		end
+
+
+
+	file.close()
+	end
+
+	def HTMLGenAnswers name
+		name+=".html"
+		cgi = CGI.new("html4")
+
+		file =  File.open("Test_" + name.gsub(/.html/, "") + "Answers.html", "w")
+		file.puts cgi.html{ cgi.body{ cgi.h2{"Test " + name.gsub(/.html/, "") + " Answers"} + "\n" + "\n" }  }
+		10.times do |i|
+			file.puts cgi.html {
+				cgi.head{
+					"\n" + cgi.title{"Test " + name.gsub(/.html/, "")}
+				} +
+				cgi.body{
+					"\n" + 
+					cgi.form{ 
+						cgi.p{(i+1).to_s + GetQuest(i, 2)}  +
+						cgi.p{GetQuest(i, 0)} + 
+						cgi.p{GetQuest(i, 1)} + cgi.br
+					}
+				}
+			}
+		end
+
+
+
+	file.close()
+	end
+
 	def GetQuest x, y												
 		@@allTests[x][y]
 	end
-	
 end
