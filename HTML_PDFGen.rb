7dff66619e16201a4c31de5729667d04a4d86417
IvanZorov <i_zoro007@abv.bg>
2014-07-22 20:20:11 +0300

    Update HTML_PDFGen.rb

--- HTML_PDFGen.rb
+++ HTML_PDFGen.rb
@@ -7,10 +7,33 @@ class HTMLPDFGen
 	@@allTests = []
 
 
-	def	GenTests
+	def GenTests
 		10.times do |i|
 			@@allTests << @@eqGen.Kvadratno
 		end																
+	end
+	
+	def PDFGen name
+		pdf = Prawn::Document.new
+
+		10.times do |i|
+			pdf.text ((i+1).to_s + GetQuest( i-1, 2))
+		end
+
+		pdf.render_file ("Test_" + name)
+	end
+
+	def PDFGenAnswers name
+		pdf = Prawn::Document.new
+
+		10.times do |i|
+			pdf.text ((i+1).to_s + GetQuest( i-1, 2))
+			pdf.text (GetQuest i-1, 0)
+			pdf.text (GetQuest i-1, 1)
+			pdf.text "\n"
+		end
+
+		pdf.render_file ("Test_" + name.gsub(/.pdf/, "") + "Answers.pdf")
 	end
 	
 	def GetQuest x, y												
